31277853192412b8b7fa6f7e6c6ebebdfcc944be
[dyad] <marcelo.net77@hotmail.com>
2025-11-29 04:00:29 +0000

    [dyad] Audited and fixed codebase - wrote 3 file(s), renamed 1 file(s), deleted 4 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/Index"; // This will be the Hero component
+import AppLayout from "./pages/AppLayout"; // This will be the main app with tabs
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -11,11 +11,11 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      <Sonner /> {/* Sonner is the preferred toast notification */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Index />} /> {/* Landing page with Hero */}
+          <Route path="/app/*" element={<AppLayout />} /> {/* Main application with tabs */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
