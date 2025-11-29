a188e12083910b52ab7fcf43059c0d3e89368a18
[dyad] <marcelo.net77@hotmail.com>
2025-11-29 04:51:16 +0000

    [dyad] Configured app for Nginx static server by switching to HashRouter - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import Index from "./pages/Index";
 import AppLayout from "./pages/AppLayout";
-import DemoLayout from "./pages/DemoLayout"; // Import the new DemoLayout
+import DemoLayout from "./pages/DemoLayout";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import { SessionContextProvider } from "./components/SessionContextProvider";
@@ -15,18 +15,18 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner />
-      <BrowserRouter>
+      <HashRouter> {/* Changed BrowserRouter to HashRouter */}
         <SessionContextProvider>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/app/*" element={<AppLayout />} />
-            <Route path="/demo/*" element={<DemoLayout />} /> {/* Add the Demo route */}
+            <Route path="/demo/*" element={<DemoLayout />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </SessionContextProvider>
-      </BrowserRouter>
+      </HashRouter> {/* Changed BrowserRouter to HashRouter */}
     </TooltipProvider>
   </QueryClientProvider>
 );
